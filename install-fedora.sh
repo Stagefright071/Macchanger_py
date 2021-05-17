be0bd7c1399b02e5857136f6db9a6b10e11517ec
Raghuram Subramani <raghus2247@gmail.com>
2021-05-17 15:57:39 +0530

    Create install-fedora.sh

---
+++ install-fedora.sh
@@ -0,0 +1,5 @@
+sudo dnf install net-tools python3 python3-pip git
+sudo python3 -m pip install -r requirements.txt
+sudo cp bin/Macchanger.py /bin/macchangerpy > /dev/null
+clear
+echo RUN macchangerpy AS ROOT TO START
