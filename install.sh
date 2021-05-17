3a7890ab01a971b81195e1b23cef60b6dda373ad
Raghuram Subramani <raghus2247@gmail.com>
2021-05-17 04:58:38 -0400

    Fixed mistake

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt install python3 python3-pip
+sudo apt install python3 python3-pip net-tools
 sudo python3 -m pip install -r requirements.txt
 sudo cp bin/Macchanger.py /bin/macchangerpy > /dev/null
 clear
