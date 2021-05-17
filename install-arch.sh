0beb68f1b0946d978dba19e89eba797261904cf6
Raghuram Subramani <raghus2247@gmail.com>
2021-05-17 16:17:57 +0530

    Update install-arch.sh

---
+++ install-arch.sh
@@ -0,0 +1,5 @@
+sudo pacman -S python3 python-pip net-tools
+sudo python3 -m pip install -r requirements.txt
+sudo cp bin/Macchanger.py /bin/macchangerpy > /dev/null
+clear
+echo RUN macchangerpy AS ROOT TO START
