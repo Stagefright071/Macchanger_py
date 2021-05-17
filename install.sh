f092873135d0f074ab1f67f51a115cde713ff1b5
Raghuram Subramani <raghus2247@gmail.com>
2021-05-17 04:45:19 -0400

    Finished install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-sudo python3 -m pip install -r requirements.txt
-sudo cp bin/Macchanger.py /bin/macchangerpy
+ sudo python3 -m pip install -r requirements.txt
+sudo cp bin/Macchanger.py /bin/macchangerpy > /dev/null
+clear
 echo RUN macchanger AS ROOT TO START
